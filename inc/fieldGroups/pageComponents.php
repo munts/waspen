2efb8623d5c00b20132d0bc25140a261a2c27632
Scott Taylor <s.taylor06@gmail.com>
2021-03-13 15:29:01 -0700

    if condition to check for page id 2 when using blockTextImageCrop and css updates

--- inc/fieldGroups/pageComponents.php
+++ inc/fieldGroups/pageComponents.php
@@ -15,14 +15,14 @@ add_action('Flynt/afterRegisterComponents', function () {
                 'type' => 'flexible_content',
                 'button_label' => __('Add Component', 'flynt'),
                 'layouts' => [
-                    Components\BlockCollapse\getACFLayout(),
+                    // Components\BlockCollapse\getACFLayout(),
                     Components\BlockImage\getACFLayout(),
                     Components\BlockImageText\getACFLayout(),
                     Components\BlockVideoOembed\getACFLayout(),
                     Components\BlockWysiwyg\getACFLayout(),
                     Components\GridImageText\getACFLayout(),
-                    Components\GridPostsLatest\getACFLayout(),
-                    Components\ListComponents\getACFLayout(),
+                    // Components\GridPostsLatest\getACFLayout(),
+                    // Components\ListComponents\getACFLayout(),
                     Components\SliderImages\getACFLayout(),
                 ]
             ]
