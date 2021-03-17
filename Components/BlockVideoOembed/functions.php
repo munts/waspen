137773a2b5b698da14a09d6bac23ddacf20d98bf
Scott Taylor <s.taylor06@gmail.com>
2021-03-17 11:27:37 -0600

    made video fields editable via the cms - try two and if else for cf7...

--- Components/BlockVideoOembed/functions.php
+++ Components/BlockVideoOembed/functions.php
@@ -3,18 +3,6 @@
 namespace Flynt\Components\BlockVideoOembed;
 
 use Flynt\FieldVariables;
-// use Flynt\Utils\Oembed;
-
-// add_filter('Flynt/addComponentData?name=BlockVideoOembed', function ($data) {
-//     $data['video'] = Oembed::setSrcAsDataAttribute(
-//         $data['oembed'],
-//         [
-//             'autoplay' => 'true'
-//         ]
-//     );
-
-//     return $data;
-// });
 
 function getACFLayout()
 {
